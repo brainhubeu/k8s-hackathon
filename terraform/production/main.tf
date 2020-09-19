bbfcdb110b0e9f62e43abcf8e6e91e2729617daa
Rafał Kostrzewski <rafal@brainhub.pl>
2020-09-19 15:26:11 +0200

    add VPC configuration

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -30,3 +30,18 @@ module "frontend" {
     aws.us-east-1 = aws.us-east-1
   }
 }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "k8s-hackathon-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs                 = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  public_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  database_subnets    = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+  elasticache_subnets = ["10.0.31.0/24", "10.0.32.0/24", "10.0.33.0/24"]
+
+  enable_nat_gateway = true
+}
